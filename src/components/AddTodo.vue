a4a95e5ce25754cf48a81233f75576f1353d1431
pamigit <pamizoki@gmail.com>
2019-09-17 23:21:11 +0200

    init commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <form>
+            <input v-model="title" type="text" name="title" placeholder="Add title..">
+            <input type="submit" value="Submit" class="btn btn-success">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+    input[type="submit"] {
+        flex: 2;
+    }
+</style>
